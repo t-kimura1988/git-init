012a742e02490d9cf4d32d9a9d2283e5db7b0321
t-kimura1988 <goen.business.managament@gmail.com>
2023-06-14 16:19:42 +0900

    seq3
    commit featureC4 1
    
    commit featureC4 1
    
    commit featureC4 1
    
    commit featureC4 1

--- test.go
+++ test.go
@@ -5,8 +5,32 @@ import "log"
 func develop() {
 	log.Println("kkk")
 	log.Println("kkk")
+	log.Println("kkk")
+	log.Println("kkk")
+	log.Println("dev5")
+	log.Println("dev5")
+	log.Println("dev6")
+	log.Println("dev7")
 }
 
 func featureA() {
 	log.Println("kkklklklk")
+	log.Println("kkklklklk3")
+	log.Println("kkklklklk4")
+	log.Println("kkklklklk4")
+	log.Println("kkklklklk4")
+	log.Println("kkklklklk4")
+	log.Println("kkklklklk4")
+}
+
+func featureC4() {
+	log.Println("kkklklklk")
+	log.Println("kkklklklk3")
+	log.Println("kkklklklk4")
+	log.Println("kkklklklk4")
+	log.Println("kkklklklk4")
+	log.Println("kkklklklk4")
+	log.Println("kkklklklk4")
+	log.Println("kkklklklk4")
+	log.Println("kkklklklk4")
 }
