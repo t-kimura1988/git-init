942aeeda067a57227a76c27edb7ca819209068af
t-kimura1988 <goen.business.managament@gmail.com>
2023-06-14 16:19:42 +0900

    seq3

--- test.go
+++ test.go
@@ -5,8 +5,20 @@ import "log"
 func develop() {
 	log.Println("kkk")
 	log.Println("kkk")
+	log.Println("kkk")
+	log.Println("kkk")
+	log.Println("dev5")
+	log.Println("dev5")
+	log.Println("dev6")
+	log.Println("dev7")
 }
 
 func featureA() {
 	log.Println("kkklklklk")
+	log.Println("kkklklklk3")
+	log.Println("kkklklklk4")
+	log.Println("kkklklklk4")
+	log.Println("kkklklklk4")
+	log.Println("kkklklklk4")
+	log.Println("kkklklklk4")
 }
